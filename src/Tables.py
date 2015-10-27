604904948daad8cfea50b8905f60c8c9dbd99157
Merrick Ryman <merrick@ctsfla.com>
2015-10-27 09:48:03 -0400

    Revised header

--- src/Tables.py
+++ src/Tables.py
@@ -1,10 +1,8 @@
 import sys
 from prettytable import from_csv
 
-# PrettyTable: Display data in tables using a beautiful ASCII format.
-__author__ = 'Merrick Ryman'
+# This file uses a third party library. PrettyTable: Display data in tables using a beautiful ASCII format.
 # Thank you luke@maurits.id.au (code.google.com) for providing the prettytable library.
-# TODO: Add sort functionality (ex. sort by URL or genre by alphabetical order)
 
 print "Run 'addCollection' to append Collection(s) to the Collections CSV file.\n"
 print "Displaying Collections... This may take awhile.\n"
