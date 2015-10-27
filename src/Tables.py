a59665e01d311c41f4550eec67abd6f3ae915e9e
Merrick Ryman <merrick@ctsfla.com>
2015-10-27 15:16:25 -0400

    Fixed if/elif/else statements.

--- src/Tables.py
+++ src/Tables.py
@@ -4,7 +4,7 @@ from prettytable import from_csv
 # This file uses a third party library. PrettyTable: Display data in tables using a beautiful ASCII format.
 # Thank you luke@maurits.id.au (code.google.com) for providing the prettytable library.
 
-# TODO: Add sort functionality (ex. sort by URL or genre by alphabetical order)
+# TODO: Add filter functionality (ex. filter search engines with genre: google facebook duckduckgo -> google duckduckgo)
 
 print "Run 'addCollection.py' to append Collection(s) to the Collections CSV file.\n"
 
@@ -26,10 +26,13 @@ def displaySorted():
   sortBy = raw_input("Select what you would like to sort by.")
   if sortBy == "1":
     print collectionsTable.get_string(sortby='URL')
-  if sortBy == "2":
+  elif sortBy == "2":
     print collectionsTable.get_string(sortby='Genre')
-  if sortBy == "3":
+  elif sortBy == "3":
     print collectionsTable.get_string(sortby='Description')
+  else:
+    print "Invalid input detected. Sorting by URL..."
+    print collectionsTable.get_string(sortby='URL')
   collectionsData.close()
   x = raw_input("Press enter to exit.")
   sys.exit("Completed.")
