93e49d5b8230271f07b2e55bd47105068bbe7196
Merrick Ryman <merrick@ctsfla.com>
2015-10-27 13:14:34 -0400

    Add sorting functionality

--- src/Tables.py
+++ src/Tables.py
@@ -6,12 +6,43 @@ from prettytable import from_csv
 
 # TODO: Add sort functionality (ex. sort by URL or genre by alphabetical order)
 
-print "Run 'addCollection' to append Collection(s) to the Collections CSV file.\n"
-print "Displaying Collections... This may take awhile.\n"
+print "Run 'addCollection.py' to append Collection(s) to the Collections CSV file.\n"
 
 collectionsData = open("Collections.csv", "r")
 collectionsTable = from_csv(collectionsData)
-print collectionsTable
-collectionsData.close()
-x = raw_input("Press enter to exit.")
-sys.exit("Completed.")
+
+
+def displayUnsorted():
+  print collectionsTable
+  collectionsData.close()
+  x = raw_input("Press enter to exit.")
+  sys.exit("Completed.")
+
+
+def displaySorted():
+  print "1. Sort by URL"
+  print "2. Sort by Genre"
+  print "3. Sort by Description"
+  sortBy = raw_input("Select what you would like to sort by.")
+  if sortBy == "1":
+    print collectionsTable.get_string(sortby='URL')
+  if sortBy == "2":
+    print collectionsTable.get_string(sortby='Genre')
+  if sortBy == "3":
+    print collectionsTable.get_string(sortby='Description')
+  collectionsData.close()
+  x = raw_input("Press enter to exit.")
+  sys.exit("Completed.")
+
+
+sortQ = raw_input("Would you like to sort? (Yy/Nn)")
+
+if sortQ == "Y" or sortQ == "y":
+  displaySorted()
+elif sortQ == "N" or sortQ == "n":
+  print "Displaying unsorted Collection... This may take awhile.\n"
+  displayUnsorted()
+else:
+  print "I'll take that as a no."
+  print "Displaying unsorted Collection... This may take awhile.\n"
+  displayUnsorted()
